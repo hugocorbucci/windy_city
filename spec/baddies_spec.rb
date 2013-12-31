e267c2f651cb1039308deaac8674cae23ae0f1f1
James Courtois <jcourtois@thoughtworks.com>
2013-12-30 18:42:26 -0600

    Fixing test

--- spec/baddies_spec.rb
+++ spec/baddies_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/baddies.rb'
 
 describe "Drunk Bears fan" do
 	it "has drank at least two beers" do
-		drunk_bears_fan_class = Baddies.drunk_bears_fans(display_name:'DCF', hp: 20, beers_drank:3).new
+		drunk_bears_fan = Baddies.drunk_bears_fans(display_name:'DCF', hp: 20, beers_drank:3).new
 		assert drunk_bears_fan.beers_drank >= 2
 	end
 end
